892611c8cb164b2c3090a4a67bffb95239961398
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-01 11:33:31 -0500

    Adds basic config reading

--- fslab_data_analytics/osrs/run.fs
+++ fslab_data_analytics/osrs/run.fs
@@ -1,24 +1,37 @@
 module fslab_data_analytics.osrs
 
 open System
+open System.IO
 open FSharp.Stats
 open FSharp.Data
 open Deedle
+open Newtonsoft.Json
+
+type FileLocations = {
+    data: string
+}
+
+type Config = {
+    fileLocations: FileLocations
+}
 
 let run argv =
+    let configText = File.ReadAllText("fslab_data_analytics/osrs/config.json")
+    let config = JsonConvert.DeserializeObject<Config>(configText)
+    printfn "%A" <| config.fileLocations.data
     let factorialOf3 = SpecialFunctions.Factorial.factorial 3
     // Retrieve data using the FSharp.Data package
     let rawData = Http.RequestString @"https://raw.githubusercontent.com/dotnet/machinelearning/master/test/data/housing.txt"
 
     // Use .net Core functions to convert the retrieved string to a stream
-    let dataAsStream = new System.IO.MemoryStream(rawData |> System.Text.Encoding.UTF8.GetBytes) 
+    let dataAsStream = new MemoryStream(rawData |> System.Text.Encoding.UTF8.GetBytes) 
 
     // And finally create a data frame object using the ReadCsv method provided by Deedle.
     // Note: Of course you can directly provide the path to a local source.
     let dataAsFrame = Frame.ReadCsv(dataAsStream,hasHeaders=true,separators="\t")
 
     // Using the Print() method, we can use the Deedle pretty printer to have a look at the data set.
-    dataAsFrame.Print()
+    // dataAsFrame.Print()
 
     let housesNotAtRiver = 
         dataAsFrame
@@ -27,12 +40,12 @@ let run argv =
 
     //sprintf "The new frame does now contain: %i rows and %i columns" housesNotAtRiver.RowCount housesNotAtRiver.ColumnCount
 
-    housesNotAtRiver.Print()
+    // housesNotAtRiver.Print()
 
     let x =
         housesNotAtRiver
         |> Frame.melt
-    x.Print()
+    // x.Print()
 
     [6.0; factorialOf3]
     |> Seq.iter (printfn "%A")
