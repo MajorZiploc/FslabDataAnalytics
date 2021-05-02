8d2bfd7cdc3b0fc63f61376b0d3935da31934dbc
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-02 06:00:36 -0500

    Adds showing col names

--- fslab_data_analytics/osrs/run.fs
+++ fslab_data_analytics/osrs/run.fs
@@ -18,34 +18,26 @@ let run argv =
     let config = JsonConvert.DeserializeObject<Config>(configText)
     utils.setFullPaths (config.fileLocations) thisDir
     printfn "%A" <| config.fileLocations.["data"]
-    let factorialOf3 = SpecialFunctions.Factorial.factorial 3
-    // Retrieve data using the FSharp.Data package
-    let rawData = Http.RequestString @"https://raw.githubusercontent.com/dotnet/machinelearning/master/test/data/housing.txt"
+    let df = Frame.ReadCsv(config.fileLocations.["data"],hasHeaders=true,separators="\t")
+    // df.Print()
 
-    // Use .net Core functions to convert the retrieved string to a stream
-    let dataAsStream = new MemoryStream(rawData |> System.Text.Encoding.UTF8.GetBytes) 
+    // Prints column names
+    printfn "%A" <| df.Columns.Keys
 
-    // And finally create a data frame object using the ReadCsv method provided by Deedle.
-    // Note: Of course you can directly provide the path to a local source.
-    let dataAsFrame = Frame.ReadCsv(dataAsStream,hasHeaders=true,separators="\t")
-
-    // Using the Print() method, we can use the Deedle pretty printer to have a look at the data set.
-    // dataAsFrame.Print()
-
-    let housesNotAtRiver = 
-        dataAsFrame
-        |> Frame.sliceCols ["RoomsPerDwelling";"MedianHomeValue";"CharlesRiver"]
-        |> Frame.filterRowValues (fun s -> s.GetAs<bool>("CharlesRiver") |> not ) 
+    // let housesNotAtRiver = 
+    //     df
+    //     |> Frame.sliceCols ["RoomsPerDwelling";"MedianHomeValue";"CharlesRiver"]
+    //     |> Frame.filterRowValues (fun s -> s.GetAs<bool>("CharlesRiver") |> not ) 
 
     //sprintf "The new frame does now contain: %i rows and %i columns" housesNotAtRiver.RowCount housesNotAtRiver.ColumnCount
 
     // housesNotAtRiver.Print()
 
-    let x =
-        housesNotAtRiver
-        |> Frame.melt
+    // let x =
+    //     housesNotAtRiver
+    //     |> Frame.melt
     // x.Print()
 
-    [6.0; factorialOf3]
-    |> Seq.iter (printfn "%A")
+    // [6.0; factorialOf3]
+    // |> Seq.iter (printfn "%A")
     0 // return an integer exit code
